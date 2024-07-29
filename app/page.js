f473ef76d87a45e6fcb04b59fc869c2e694ab7fe
Antonio Vass <152070597+saint-simone@users.noreply.github.com>
2024-07-29 11:26:47 +0200

    modified:   app/page.js 	new file:   components/BestSeller/BestSeller.js 	new file:   components/ProductCard/ProductCard.js 	new file:   components/ui/aspect-ratio.jsx 	new file:   lib/utils.js 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/AmericanoCap.js 	new file:   pages/AspenBeanie.js 	new file:   pages/ClassicTShirt.js 	new file:   pages/PromoTShirt.js 	modified:   tailwind.config.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import Hero from "./navbar/Hero/page";
+import BestSeller from "../components/BestSeller/BestSeller.js"
+import Hero from "./Navbar/Hero/page.js";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <main>
         {" "}
         <Hero />
+        <BestSeller />
       </main>
     </>
   );
---
+++ components/BestSeller/BestSeller.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AmericanoCap from "@/pages/AmericanoCap";
+import AspenBeanie from "@/pages/AspenBeanie";
+import ClassicTShirt from "@/pages/ClassicTShirt";
+import PromoTShirt from "@/pages/PromoTShirt";
+
+const BestSeller = () => {
+  return (
+    <section className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-2">
+        <h2 className="text-3xl sm:font-medium sm:text-2xl md:text-3xl lg:text-3xl  text-center text-red-600 font-semibold">
+          BEST SELLERS
+        </h2>
+      </div>
+      <div className="lg:ml-11 lg:mr-12 p-12 mb-10 lg:mt-0 grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-2 grid-cols-2 sm:gap-4 gap-2">
+        <>
+          <AmericanoCap />
+          <AspenBeanie />
+          <ClassicTShirt />
+          <PromoTShirt />
+        </>
+      </div>
+    </section>
+  );
+};
+
+export default BestSeller;
