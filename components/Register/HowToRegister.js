55963179f8528d14e5077a9a1ba2d2af3c5e79a7
Wilkho Tshink <stwilkho@gmail.com>
2024-07-31 11:13:55 +0200

    modified:   components/Register/HowToRegister.js

--- components/Register/HowToRegister.js
+++ components/Register/HowToRegister.js
@@ -1,19 +1,24 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
- const HowToRegister = () => {
+const HowToRegister = () => {
   return (
     <div>
       <section className="bg-black text-center text-white py-16">
         <h1 className="text-2xl font-bold">HOW TO REGISTER?</h1>
         <p className="mt-4 max-w-2xl mx-auto">
-          Captivity is strictly distributed via authorised distributors namely: promotional companies, advertising and event companies as well as independent resellers.
-          We have a large offering of headwear and apparel and are distributors of a large selection of promotional items.
-          Should you fall into either of these categories and would like to register as a vendor?
+          Captivity is strictly distributed via authorised distributors namely:
+          promotional companies, advertising and event companies as well as
+          independent resellers. We have a large offering of headwear and
+          apparel and are distributors of a large selection of promotional
+          items. Should you fall into either of these categories and would like
+          to register as a vendor?
         </p>
-        <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded">
-          Register Here
-        </button>
+        <Link>
+          <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded">
+            Register Here
+          </button>
+        </Link>
       </section>
 
       <section className="bg-white py-16">
@@ -30,18 +35,23 @@ import Link from 'next/link'
             </Link>
           </div>
           <div className="lg:w-1/2 mt-8 lg:mt-0 lg:ml-8">
-            <h2 className="text-red-600 text-2xl font-bold">VISIT THE FWRD WEBSITE</h2>
+            <h2 className="text-red-600 text-2xl font-bold">
+              VISIT THE FWRD WEBSITE
+            </h2>
             <p className="mt-4 text-gray-600">
-              The FWRD Brand is distributed via promotional companies, advertising, event companies and resellers ONLY.
+              The FWRD Brand is distributed via promotional companies,
+              advertising, event companies and resellers ONLY.
             </p>
-            <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded">
-              Visit Site
-            </button>
+            <Link>
+              <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded">
+                Visit Site
+              </button>
+            </Link>
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default HowToRegister;
