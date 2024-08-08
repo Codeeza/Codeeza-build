7c0ced9e219e6a485366cc8371d4f3841fda11ba
Antonio Vass <152070597+saint-simone@users.noreply.github.com>
2024-08-08 14:26:06 +0200

    modified:   app/products/basic-sweater/page.js 	modified:   app/products/ice-beanie/page.js 	modified:   app/products/long-sleeve-golfer/page.js 	modified:   app/products/puffer-jacket/page.js

--- app/products/ice-beanie/page.jsx
+++ app/products/ice-beanie/page.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:app/products/ice-beanie/page.jsx
 "use client"
 
 import React from "react";
@@ -15,3 +16,32 @@ const page = () => {
 };
 
 export default page;
+=======
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-4">
+      <div className="text-sm text-gray-500">
+        <Link href="/" className="text-gray-500 hover:text-gray-700">
+          Home
+        </Link>{" "}
+        /{" "}
+        <Link
+          href="/collections/winter-collection"
+          className="text-gray-500 hover:text-gray-700"
+        >
+          Winter Collection
+        </Link>{" "}
+        / <span className="text-black font-semibold">Ice Beanie</span>
+        <h2 className="text-4xl font-medium text-red-600 mt-4">ICE BEANIE</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
+>>>>>>> cb3de90 (	modified:   app/products/basic-sweater/page.js):app/products/ice-beanie/page.js
--- app/products/puffer-jacket/page.jsx
+++ app/products/puffer-jacket/page.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:app/products/puffer-jacket/page.jsx
 "use client";
 
 import React from "react";
@@ -17,3 +18,34 @@ const page = () => {
 };
 
 export default page;
+=======
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-4">
+      <div className="text-sm text-gray-500">
+        <Link href="/" className="text-gray-500 hover:text-gray-700">
+          Home
+        </Link>{" "}
+        /{" "}
+        <Link
+          href="/collections/winter-collection"
+          className="text-gray-500 hover:text-gray-700"
+        >
+          Winter Collection
+        </Link>{" "}
+        / <span className="text-black font-semibold">Alaska Puffer Jacket</span>
+        <h2 className="text-4xl font-medium text-red-600 mt-4">
+          ALASKA PUFFER JACKET
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
+>>>>>>> cb3de90 (	modified:   app/products/basic-sweater/page.js):app/products/puffer-jacket/page.js
