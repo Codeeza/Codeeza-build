761e35838b17b8cab86fafa5c597445e3ff6bca5
Tyler Van Wyk <tylervanwyk01@outlook.com>
2024-08-08 12:38:04 +0200

    modified:   components/Footer/Footer.js [tfvw]

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
             <li className="mb-2 flex items-center">
               <FaYoutube className="icons youtube text-white hover:text-gray-500" />
               <a
-                href="https://youtube.com"
+                href="https://www.youtube.com/channel/UCCY1jcwbOK-DXEfmMI5a77Q"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-2 hover:text-red-600"
