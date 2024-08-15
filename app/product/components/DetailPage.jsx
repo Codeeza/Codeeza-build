e5263e8e238a5e0d828dc6b87ce2cd72798f20cc
Pencils0101001101001010 <155194934+Pencils0101001101001010@users.noreply.github.com>
2024-08-15 19:24:51 +0200

    modified:   app/product/components/DetailPage.jsx 	modified:   app/product/components/ProductModal.jsx 	modified:   lib/db.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/Original-Trucker-Camo-Green-Soc1.jpg 	new file:   public/Original-Trucker-Camo-Woodland-Boc1.jpg 	new file:   public/Original-Trucker-Camo-forrest-Foc1.jpg 	renamed:    public/ice-beanie-dusty-pink-(1).jpg -> public/ice-beanie-dusty-pink-.jpg

--- app/product/components/DetailPage.jsx
+++ app/product/components/DetailPage.jsx
@@ -1,120 +1,146 @@
 "use client";
 import { useState, useEffect } from "react";
-import ProductModal from "./ProductModal";
 import Image from "next/image";
+import ProductModal from "./ProductModal";
 import Link from "next/link";
-import { countries } from "@/lib/CountrySelect";
 
 const ProductPage = ({ product }) => {
   const [selectedVariant, setSelectedVariant] = useState(null);
-  const [selectedImage, setSelectedImage] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [mainImage, setMainImage] = useState(null);
 
   useEffect(() => {
-    if (product.colorVariants && product.colorVariants.length > 0) {
-      setSelectedVariant(product.colorVariants[0]);
+    if (product.variants && product.variants.length > 0) {
+      setSelectedVariant(product.variants[0]);
+      setMainImage(product.variants[0].image);
     }
   }, [product]);
 
   const handleVariantChange = (variant) => {
     setSelectedVariant(variant);
+    setMainImage(variant.image);
   };
 
-  // useEffect(() => {
-  //   if (product.imageSelect && product.colorVariants.length > 0) {
-  //     setSelectedImage(product.colorVariants[0]);
-  //   }
-  // }, [product]);
-
-  // const handleImageChange = (image) => {
-  //   setSelectedImage(image);
-  // }
+  const handleImageClick = (image) => {
+    setMainImage(image);
+  };
 
   return (
-    <div className="p-6">
-      <div className="ml-2 sm:ml-28 ">
-        {" "}
-        <h1 className="text-3xl text-red-500 font-bold">{product.name}</h1>
-        <div className="flex items-center  space-x-2 text-sm text-gray-600">
-          <span>Home</span> / <span>Headwear</span> /{" "}
-          <span>{product.name}</span>
-        </div>
-      </div>
-      <div className=" mt-8 flex-wrap sm:flex justify-center">
-        <div className="w-full sm:w-[500px]">
-          {selectedVariant && (
-            <Image
-              src={selectedVariant.image}
-              alt={`${product.name} in ${selectedVariant.name}`}
-              width={500}
-              height={500}
-              className="w-full"
-            />
+    <div className="p-4 md:p-6 max-w-7xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-center">
+        <div className="w-full md:w-1/3 mb-6 md:mb-0">
+          <div className="mb-6">
+            <h1 className="text-2xl md:text-3xl font-bold text-red-500">
+              {product.name}
+            </h1>
+            <div className="flex items-center space-x-2 text-sm text-gray-600 flex-wrap">
+              <span>Home</span> / <span>headwear</span> /{" "}
+              <span>{product.name}</span>
+            </div>
+          </div>
+          {mainImage && (
+            <div className="mb-4">
+              <Image
+                src={mainImage}
+                alt={`${product.name} - Main Image`}
+                width={700}
+                height={700}
+                className="w-full h-auto"
+              />
+            </div>
           )}
-
-          {/* <div className="mt-4">
-            <button className="w-full p-2 bg-blue-500 text-white rounded">
-              View full image gallery
-            </button>
-          </div> */}
+          <div className="flex flex-wrap gap-2 justify-center">
+            {product.variants &&
+              product.variants.map((variant) => (
+                <button
+                  key={variant.name}
+                  onClick={() => handleVariantChange(variant)}
+                  className=""
+                  aria-label={`Select ${variant.name}`}
+                >
+                  {variant.type === "image" ? (
+                    <Image
+                      src={variant.image}
+                      alt={variant.name}
+                      width={100}
+                      height={100}
+                      className="w-12 h-12 md:w-16 md:h-16 object-cover opacity-50 hover:opacity-100"
+                      onClick={() => handleImageClick(variant.image)}
+                    />
+                  ) : null}
+                </button>
+              ))}
+          </div>
         </div>
-
-        <div className="w-1/2 px-6 text-gray-700">
+        <div className="w-full md:w-1/3 mt-5 sm:mt-20 text-sm md:text-base text-gray-700 md:pl-6">
           {selectedVariant && (
             <>
-              <p>
-                <Link
-                  href="/login"
-                  className="font-bold text-red-500 hover:text-gray-700  "
-                >
-                  Login or Register
-                </Link>{" "}
-                to view product prices
-              </p>
+              <Link
+                href="/login"
+                className="font-bold text-red-500 hover:text-gray-700"
+              >
+                Login or Register
+              </Link>{" "}
+              to view product prices
               <p className="mt-2">
-                <strong>Color:</strong> {selectedVariant.name}
+                <strong>
+                  {selectedVariant.type === "Style" ? "Color" : "Color"}:
+                </strong>{" "}
+                {selectedVariant.name}
               </p>
-              <div className="flex mt-4 mb-4 ml-5 space-x-2 flex-wrap">
-                {product.colorVariants &&
-                  product.colorVariants.map((variant) => (
+              {product.variants &&
+                product.variants.map((variant) => (
+                  <button
+                    key={variant.name}
+                    onClick={() => handleVariantChange(variant)}
+                    className=""
+                    aria-label={`Select ${variant.name}`}
+                  >
+                    {variant.type === "color" ? (
+                      <div
+                        className="w-8 h-8 m-0.5"
+                        title={variant.name}
+                        style={{ backgroundColor: variant.hex }}
+                      ></div>
+                    ) : null}
+                  </button>
+                ))}
+              <div className="flex flex-wrap gap-2 my-4">
+                {product.thumbnails &&
+                  product.thumbnails.map((thumbnail) => (
                     <button
-                      key={variant.name}
-                      onClick={() => handleVariantChange(variant)}
-                      className={`w-8 h-8 border-2 p-2 m-1 ${
-                        selectedVariant && selectedVariant.name === variant.name
-                          ? "ring-2 ring-red-500"
-                          : ""
-                      }`}
-                      style={{ backgroundColor: variant.hex }}
-                      aria-label={`Select ${variant.name}`}
-                      title={`${variant.name}`}
-                    ></button>
+                      key={thumbnail.name}
+                      onClick={() => handleVariantChange(thumbnail)}
+                      className="w-12 h-12 md:w-16 md:h-16"
+                      aria-label={`Select ${thumbnail.name}`}
+                    >
+                      <Image
+                        src={thumbnail.image}
+                        alt={thumbnail.name}
+                        width={100}
+                        height={100}
+                        className="w-full h-full object-cover opacity-50 hover:opacity-100"
+                        onClick={() => handleImageClick(thumbnail.image)}
+                      />
+                    </button>
                   ))}
               </div>
-              <p className="text-sm mt-5">
-                {/* CODE: {selectedVariant.code} */}
-                <strong>SIZE</strong>: {selectedVariant.size}
-              </p>
-              {/* <p className="mt-4 text-lg">Price: {selectedVariant.price}</p> */}
-              <p className="mt-2 text-sm mb-4">
-                {selectedVariant.stock} in stock
-              </p>
+              <p>in stock {selectedVariant.stock}</p>
             </>
           )}
-          {/* <p className="mt-4 mb-4 ">{product.description}</p> */}
-          <span className="">
-            <input
-              type="number"
-              className="bg-white mb-2 w-[50px] border-2 border-gray-300"
-            />
-          </span>
-          <div>
+          <div className="flex flex-col space-y-4 mt-6">
             <Link
               href="/login"
-              className="w-[200px] p-2 hover:bg-red-700 bg-red-500 text-white rounded mb-2"
+              className="w-full md:w-auto px-4 py-2 text-center bg-red-500 hover:bg-red-700 text-white rounded"
             >
-              login
+              Login
             </Link>
+            <button
+              className="w-full md:w-auto px-4 py-2 text-center border-red-500 border-2 text-red-500 rounded hover:bg-gray-200 transition-colors"
+              onClick={() => setShowModal(true)}
+            >
+              View Stock
+            </button>
           </div>
         </div>
       </div>
--- app/product/components/ProductModal.jsx
+++ app/product/components/ProductModal.jsx
@@ -1,44 +1,45 @@
 const ProductModal = ({ product, setShowModal }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white rounded-lg w-3/4 p-6">
+    <div className="fixed inset-0 p-5 h-full bg-gray-300 bg-opacity-50 flex justify-center items-center -4">
+      <div className=" bg-white m-5 rounded-lg w-full h-auto max-h-[750px] max-w-4xl overflow-scroll p-4 sm:p-6 sm:m-5 mt-5">
         <div className="flex justify-between items-center">
-          <h2 className="text-xl font-bold">{product.name} - Stock Details</h2>
-          <button onClick={() => setShowModal(false)} className="text-gray-500">
+          <h2 className="text-lg sm:text-xl font-bold">
+            {product.name} - Stock Details
+          </h2>
+          <button
+            onClick={() => setShowModal(false)}
+            className="text-gray-500 text-2xl"
+          >
             &times;
           </button>
         </div>
         <div className="mt-4">
-          <div className="flex space-x-4">
-            {product.colors.map((color) => (
-              <div key={color.name} className="w-1/2">
-                <h3 className="font-bold">{color.name}</h3>
-                <table className="table-auto w-full mt-2 text-sm text-left">
-                  <thead>
-                    <tr>
-                      <th className="px-2 py-1 border-b">Size</th>
-                      <th className="px-2 py-1 border-b">Stock on Hand</th>
-                      <th className="px-2 py-1 border-b">Reserved</th>
-                      <th className="px-2 py-1 border-b">Incoming</th>
-                      <th className="px-2 py-1 border-b">ETA</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {product.sizes.map((size) => (
-                      <tr key={size}>
-                        <td className="px-2 py-1 border-b">{size}</td>
-                        <td className="px-2 py-1 border-b">1234</td>{" "}
-                        {/* Replace with real data */}
-                        <td className="px-2 py-1 border-b">567</td>{" "}
-                        {/* Replace with real data */}
-                        <td className="px-2 py-1 border-b">890</td>{" "}
-                        {/* Replace with real data */}
-                        <td className="px-2 py-1 border-b">21 Aug 2024</td>{" "}
-                        {/* Replace with real data */}
+          <div className="grid grid-cols-1 gap-4">
+            {product.variants.map((variant) => (
+              <div key={variant.name}>
+                <h3 className="font-bold">{variant.name}</h3>
+                <div className="overflow-x-auto">
+                  <table className="w-full mt-2 text-sm text-left">
+                    <thead>
+                      <tr>
+                        <th className="px-2 py-1 border-b">Size</th>
+                        <th className="px-2 py-1 border-b">Stock on Hand</th>
+                        <th className="px-2 py-1 border-b">Reserved</th>
+                        <th className="px-2 py-1 border-b">Incoming</th>
+                        <th className="px-2 py-1 border-b">ETA</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td className="px-2 py-1 border-b">{variant.size}</td>
+                        <td className="px-2 py-1 border-b">{variant.stock}</td>
+                        <td className="px-2 py-1 border-b">567</td>
+                        <td className="px-2 py-1 border-b">890</td>
+                        <td className="px-2 py-1 border-b">21 Aug 2024</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             ))}
           </div>
