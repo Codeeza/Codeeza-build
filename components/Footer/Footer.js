12bf9b30aa797b6ebf5124af20a25578c564d6a4
Wilkho Tshink <stwilkho@gmail.com>
2024-08-19 13:55:45 +0200

    modified:   components/Footer/Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -109,7 +109,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center mt-10">
         <Link href="https://captivity.co.za/contact/">
           <button className="bg-transparent border-2 border-slate-400 text-white hover:bg-red-700 hover:border-red-700 py-2 px-4">
             OUR OFFICES
